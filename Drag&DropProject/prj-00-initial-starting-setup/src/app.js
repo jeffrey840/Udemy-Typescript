53b5d7950836c48445c70a9e628088db8c954d3d
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-12-19 03:34:12 -0600

    added form

---
+++ Drag&DropProject/prj-00-initial-starting-setup/src/app.js
@@ -0,0 +1,15 @@
+var ProjectInput = /** @class */ (function () {
+    function ProjectInput() {
+        //will not be null and will be of type element
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+        var importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    ProjectInput.prototype.attach = function () {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    };
+    return ProjectInput;
+}());
+var prjInput = new ProjectInput();
